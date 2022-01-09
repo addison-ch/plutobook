dadd598dd26ae8b8c548031ad8eb1d858245da96
Addison <74084822+addison-ch@users.noreply.github.com>
2022-01-08 23:20:36 -0500

    add small changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    return <h1> hi </h1>
+
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const onClick = () => {
+        console.log(input)
+    }
+
+    return <div>
+        <textarea value={input} onChange={e => setInput(e.target.value)}>
+
+        </textarea>
+        <div>
+            <button onClick={onClick}> Submit
+            </button>
+        </div>
+        <pre>{code}</pre>
+    </div>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
