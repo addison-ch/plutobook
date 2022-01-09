64eabba9474a53c809d4b3c992c2e134b73db45b
Addison <74084822+addison-ch@users.noreply.github.com>
2022-01-09 18:57:56 -0500

    fully implement code transpiling for jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,38 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom'
 
 import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
 
+    const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
     const startService = async () => {
-        const service = await esbuild.startService({
+        ref.current = await esbuild.startService({
             worker: true,
             wasmURL: '/esbuild.wasm'
         })
-        console.log(service)
+
     }
+
     useEffect(() => {
         startService();
 
     }, [])
-    const onClick = () => {
-        console.log(input)
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+
+        setCode(result.code);
     }
 
     return <div>
